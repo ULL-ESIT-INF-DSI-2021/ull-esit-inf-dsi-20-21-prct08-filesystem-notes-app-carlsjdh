cee8ad830e834b1e53f2593fcc01a4449ecec33a
Carlos Delgado <alu0101016054@ull.edu.es>
2021-04-21 21:19:43 +0100

    Some test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -5,7 +5,6 @@ import * as fs from 'fs';
 
 
 describe(('Inital test'), () => {
-  fs.rmdirSync(`./notes`, {recursive: true});
   const notes :Notes = Notes.getNotes();
   it(('Se instancia una clase Notes correctamente'), () => {
     expect(notes instanceof Notes).to.be.equal(true);
