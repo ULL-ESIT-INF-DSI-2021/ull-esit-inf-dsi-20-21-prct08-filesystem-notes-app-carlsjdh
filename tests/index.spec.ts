b3e28c03319e59feb683a2f4be47e8e09744204f
Carlos Delgado <alu0101016054@ull.edu.es>
2021-04-21 21:16:10 +0100

    Some test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,8 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
+import {Notes} from '../src/notes';
+import * as fs from 'fs';
+
 
 describe(('Inital test'), () => {
-  it(('Test 1'), () => {
-    expect(true).to.be.equal(true);
+  fs.rmdirSync(`./notes`, {recursive: true});
+  const notes :Notes = Notes.getNotes();
+  it(('Se instancia una clase Notes correctamente'), () => {
+    expect(notes instanceof Notes).to.be.equal(true);
   });
 });
